496044bb7500100e0fe83f77b6e1e6d534150d4c
phoenixjw <phoenixjw9320@gmail.com>
2023-05-22 20:02:41 +0100

    Setup components and export

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as ChannelDetail } from './ChannelDetail';
+export { default as SearchFeed} from './SearchFeed';
+export { default as VideoDetail } from './VideoDetail';
+
+// Allows for cleaner imports into App.js file, all components imported on 1 line
