e8c8a6b407edcd5dc6c38504fb57d50de9f8b3f3
phoenixjw <phoenixjw9320@gmail.com>
2023-05-25 11:01:16 +0100

    exported components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ export { default as Feed } from './Feed';
 export { default as ChannelDetail } from './ChannelDetail';
 export { default as SearchFeed} from './SearchFeed';
 export { default as VideoDetail } from './VideoDetail';
+export { default as SearchBar } from './SearchBar';
+export { default as Sidebar } from './Sidebar';
+export { default as Videos } from './Videos';
 
 // Allows for cleaner imports into App.js file, all components imported on 1 line
