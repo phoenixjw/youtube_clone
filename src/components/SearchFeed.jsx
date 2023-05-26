856bd26304a121c73429d234ce8acf985ee766e2
phoenixjw <phoenixjw9320@gmail.com>
2023-05-26 20:17:43 +0100

    Commit for deployment

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import Videos from './Videos';
+import { fetchFromAPI } from '../utils/fetchFromAPI'; 
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams()
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data)=> setVideos(data.items))
+  }, [searchTerm])
+
   return (
-    <div>SearchFeed</div>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '9', flex: 2}}>
+        <Typography varaint="h4" fontWeight="bold" mb={2} sx={{ color: 'white'}}>
+          Search Results for:  <span style={{ color: '#F31503'}}>{searchTerm}</span> videos
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,7 @@ export { default as VideoDetail } from './VideoDetail';
 export { default as SearchBar } from './SearchBar';
 export { default as Sidebar } from './Sidebar';
 export { default as Videos } from './Videos';
+export { default as VideoCard } from './VideoCard';
+export { default as ChannelCard } from './ChannelCard';
 
 // Allows for cleaner imports into App.js file, all components imported on 1 line
